54bccdaa1e4772ca2f90b5762595f9f3968a41cd
Ori Bennett <oribennett@gmail.com>
2024-01-26 15:42:26 +0200

    More installs for MUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,11 @@ import { useState } from "react";
 import seeds from "./lib/seeds";
 import Canvas from "./components/Canvas";
 import Link from "next/link";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 
 export default function Home() {
