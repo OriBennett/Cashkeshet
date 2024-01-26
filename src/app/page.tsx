8c9e2d7270e01dc13e3b27cbb2eb9d4f5eaac028
Arik Alexandrov <arikxl@gmail.com>
2024-01-26 14:15:23 +0200

    arik test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import ArikTest from "./components/ArikTest";
 
 export default function Home() {
   return (
@@ -9,6 +10,8 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
+
+        <ArikTest/>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
