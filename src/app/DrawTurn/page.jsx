dcca59b92e53e3ec114b80b6406bf6d51a581adf
Ori Bennett <oribennett@gmail.com>
2024-01-26 16:26:10 +0200

    moved Canvas to Draw page

--- src/app/DrawTurn/page.jsx
+++ src/app/DrawTurn/page.jsx
@@ -1,8 +1,23 @@
+"use client"
 import Link from "next/link";
+import { useState } from "react";
+import seeds from "../lib/seeds";
+import Canvas from "../components/Canvas";
 
 const DrawTurnPage = () => {
+  const [scribbleExists, setScribbleExists] = useState(false);
+  const [seed] = useState(seeds[Math.floor(Math.random() * seeds.length)]);
+
+  const [scribble, setScribble] = useState(null);
+
   return (
     <div>
+      <Canvas
+            startingPaths={seed.paths}
+            onScribble={setScribble}
+            scribbleExists={scribbleExists}
+            setScribbleExists={setScribbleExists}
+          />
 
       DrawTurnPage
       <hr />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
 "use client"
-import Image from "next/image";
-import { useState } from "react";
-import seeds from "./lib/seeds";
-import Canvas from "./components/Canvas";
 import Link from "next/link";
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-
-
-export default function Home() {
-  const [scribbleExists, setScribbleExists] = useState(false);
-  const [seed] = useState(seeds[Math.floor(Math.random() * seeds.length)]);
-
-  const [scribble, setScribble] = useState(null);
-
+export default function Home() {  
   return (
     <main >
       <h1>Welcome to CashCashIt</h1>
@@ -29,12 +18,7 @@ export default function Home() {
         <button>Cachkesh It!</button>
       </Link>
 
-      {/* <Canvas
-            startingPaths={seed.paths}
-            onScribble={setScribble}
-            scribbleExists={scribbleExists}
-            setScribbleExists={setScribbleExists}
-          /> */}
+      {/*  */}
 
     </main>
   );
